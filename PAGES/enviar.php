9c8ccd4aa43fc4013b3b95ac7305b18983300271
SacchettiCarla <carla.sacchetti@hotmail.com>
2022-11-20 20:55:39 -0300

    formu

--- PAGES/enviar.php
+++ PAGES/enviar.php
@@ -3,7 +3,7 @@ $name = $_POST['nombre'];
 $email = $_POST['email'];
 $message = $_POST['mensaje'];
 
-$header = 'De: ' . $email . " \r\n";
+$header = 'from:' . $email . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
@@ -13,7 +13,7 @@ $message .= "Su e-mail es: " . "$email" . " \r\n";
 $message .= "Mensaje: " . "$_POST['mensaje']" . " \r\n";
 $message .= "Enviado el " . date('d/m/Y', time());
 
-$for = 'mimail@mail.com';
+$for = 'carla.sacchetti@hotmail.com';
 $subjet = 'Mensaje de asunto';
 
 mail($for, $subjet, utf8_decode($message), $header);
